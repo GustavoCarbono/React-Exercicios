750536850821e4bd4d6a847c22280f8c9c0053e4
GustavoCarbono <gustavo.s.c.820@gmail.com>
2026-03-13 12:11:24 -0300

    estilizando e colocando imagem na lista

--- lista-react/App.js
+++ lista-react/App.js
@@ -1,11 +1,23 @@
-import {View, FlatList, StyleSheet, Text, StatusBar} from 'react-native';
+import {View, FlatList, StyleSheet, Image, Text, StatusBar} from 'react-native';
 import {DATA} from "./bd.js"
 
-const Item = ({nomeProduto, dataCadastro, valor, qtd}) => (
+const Imagens = {
+  batata: require("./imagens/batata.png"),
+  tomate: require("./imagens/tomate.png"),
+  alface: require("./imagens/alface.png"),
+  cenoura: require("./imagens/cenoura.png"),
+  banana: require("./imagens/banana.png")
+}
+
+const Item = ({nomeProduto, dataCadastro, imagem, valor, qtd}) => (
   <View style={styles.item}>
     <Text style={styles.title}>{nomeProduto}</Text>
+    <View style={styles.imagem_view}>
+      <Image source={Imagens[imagem]} 
+      style={{ width: 200, height: 200 }}/>
+    </View>
     <Text style={styles.title}>{dataCadastro}</Text>
-    <Text style={styles.title}>{valor}</Text>
+    <Text style={styles.title}>R${valor}</Text>
     <Text style={styles.title}>{qtd}</Text>
   </View>
 );
@@ -15,7 +27,7 @@ return(
   <View style={styles.container}>
     <FlatList
       data={DATA}
-      renderItem={({item}) => <Item nomeProduto={item.nomeProduto} dataCadastro={item.dataCadastro} valor={item.valor} qtd={item.qtd} />}
+      renderItem={({item}) => <Item nomeProduto={item.nomeProduto} dataCadastro={item.dataCadastro} imagem={item.imagem} valor={item.valor} qtd={item.qtd} />}
       keyExtractor={item => item.id}
     />
   </View>
@@ -24,16 +36,31 @@ return(
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+    justifyContent: "center",
+    alignContent: "center"
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: '#28d84e',
     padding: 20,
     marginVertical: 8,
+    width: 300,
     marginHorizontal: 16,
+    borderRadius: 8
+  },
+  imagem_view: {
+    backgroundColor: 'white',
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    padding: 10,
+    width: 230,
+    borderRadius: 8,
   },
   title: {
     fontSize: 32,
+    color: "white",
   },
 });
